6ac0c521486c85c7b1c8bf98b65915c0b9398514
Gustavo Del Razo <1722110326@utectulancingo.edu.mx>
2024-03-16 03:42:29 +0000

    ya

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-"""Framework web.py"""
 import web
 
-# Rutas de los controladores
 urls = (
     '/', 'mvc.controllers.index.Index',
     '/ver','mvc.controllers.ver.Ver',
@@ -11,7 +9,6 @@ urls = (
 )
 app = web.application(urls, globals())
 
-        
-# Punto de entrada 
+ 
 if __name__ == "__main__":
     app.run()
